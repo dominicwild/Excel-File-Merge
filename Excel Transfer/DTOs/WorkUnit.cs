0ef967e85754d04b4ba71e8298ffd24915d4bc31
dwild8 <dwild8@dxc.com>
2020-06-14 21:57:12 +0100

    Populates objects with data

---
+++ Excel Transfer/DTOs/WorkUnit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Transfer.DTOs {
+    class WorkUnit {
+
+        public Person person { get; set; }
+        public Project project { get; set; }
+        public Dictionary<DateTime, double> workLog { get; set; } = new Dictionary<DateTime, double>();
+
+        public void addWork(DateTime date, double hoursWorked) {
+            if (workLog.ContainsKey(date)) {
+                double hours = workLog[date];
+                workLog[date] = hours + hoursWorked;
+            } else {
+                workLog[date] = hoursWorked;
+            }
+        }
+
+    }
+}
--- Excel Transfer/Driver.cs
+++ Excel Transfer/Driver.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Office.Interop.Excel;
+﻿using Excel_Transfer.DTOs;
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -9,8 +10,12 @@ using System.Threading.Tasks;
 namespace Excel_Transfer {
     class Driver {
 
+        private const string PERSON_ID = "Pers.No.";
+        private const string PROJECT_ID = "Project number";
+
         Dictionary<int, Person> people = new Dictionary<int, Person>();
         Dictionary<int, Project> projects = new Dictionary<int, Project>();
+        Dictionary<string, WorkUnit> workUnits = new Dictionary<string, WorkUnit>();
         Excel labour;
 
         public void run(Application excel) {
@@ -40,11 +45,33 @@ namespace Excel_Transfer {
             for (int i = 2; i < lastRow; i++) {
                 populatePerson(i);
                 populateProject(i);
+                populateWorkUnit(i);
             }
         }
 
+        public void populateWorkUnit(int row) {
+
+            int personId = (int)labour.get(PERSON_ID, row);
+            int projectId = (int)labour.get(PROJECT_ID, row);
+            string workUnitId = $"{personId}{projectId}";
+
+            if (!workUnits.ContainsKey(workUnitId)) {
+                workUnits[workUnitId] = new WorkUnit {
+                    person = people[personId],
+                    project = projects[projectId]
+                };
+            }
+
+            WorkUnit unit = workUnits[workUnitId];
+            DateTime date = (DateTime)labour.get("Week ending", row);
+            double hoursWorked = (double)labour.get("Project time", row);
+
+            unit.addWork(date, hoursWorked);
+
+        }
+
         public void populatePerson(int row) {
-            int id = (int)labour.get("Pers.No.", row);
+            int id = (int)labour.get(PERSON_ID, row);
             if (!people.ContainsKey(id)) {
                 Person p = new Person(id);
                 string fullName = labour.get("Name of employee or applicant", row);
@@ -59,7 +86,7 @@ namespace Excel_Transfer {
         }
 
         public void populateProject(int row) {
-            int id = (int)labour.get("Project number", row);
+            int id = (int)labour.get(PROJECT_ID, row);
             if (!projects.ContainsKey(id)) {
                 Project p = new Project(id) {
                     name = labour.get("Description", row)
